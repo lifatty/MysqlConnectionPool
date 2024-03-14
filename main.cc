b53733ed4ecc4d602680f923119df87016e2c0b8
lifatty <littfatty@gmail.com>
2024-03-13 19:00:05 -0700

    2

--- main.cc
+++ main.cc
@@ -5,21 +5,21 @@
 int main()
 {
     clock_t begin = clock();
-    //Connectionpool *cp = Connectionpool::getConnectionpool();
+    Connectionpool *cp = Connectionpool::getConnectionpool();
     for(int i = 0; i < 1000; ++i)
     {
-        Connection conn;
-        char sql[1024] = {0};
-        sprintf(sql,"insert into user(name,age,sex) values('%s',%d, '%s')","zhangsan", 18, "male");
-        //std::cout<<sql<<std::endl;
-        
-        conn.connect("localhost",3306,"root", "scc2002", "chat");
-        conn.updated(sql);
-
-        // std::shared_ptr<Connection> sp = cp->getConnection();
+        // Connection conn;
         // char sql[1024] = {0};
         // sprintf(sql,"insert into user(name,age,sex) values('%s',%d, '%s')","zhangsan", 18, "male");
-        // sp->updated(sql);
+        // //std::cout<<sql<<std::endl;
+        
+        // conn.connect("localhost",3306,"root", "scc2002", "chat");
+        // conn.updated(sql);
+
+        std::shared_ptr<Connection> sp = cp->getConnection();
+        char sql[1024] = {0};
+        sprintf(sql,"insert into user(name,age,sex) values('%s',%d, '%s')","zhangsan", 18, "male");
+        sp->updated(sql);
     }
     clock_t end = clock();
     std::cout << (end - begin) << "ms" <<std::endl;
